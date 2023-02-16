b0b9a381fe34e8dca0a38f550854afef6e775a6f
Gongdongjang <jeesoo0908@gmail.com>
2023-02-17 02:57:42 +0900

    conflict test

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const express = require('express');
+const express = require("express");
 
 // Constants
 const PORT = process.env.PORT || 80;
 
 // App
 const app = express();
-app.get('/', function (req, res) {
-  res.send('Bye\n');
+app.get("/", function (req, res) {
+  res.send("Bye\n");
 });
 
 app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+//console.log();
+console.log("Running on http://localhost:" + PORT);
